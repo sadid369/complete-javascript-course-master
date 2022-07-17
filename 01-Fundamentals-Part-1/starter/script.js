f1aa6fac121b27561ea6e3aa5b0a1aff74ba5a6e
Sadid <sadid.jones@gmail.com>
2022-07-17 23:33:53 +0600

    @17/7/2022

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,32 @@
+// challenge #1
+
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
+// const massMark = 95;
+// const heightMark = 1.88;
+// const massJohn = 85;
+// const heightJohn = 1.76;
+// const BMIMark = massMark / heightMark ** 2;
+// const BMIJohn = massJohn / heightJohn ** 2;
+// const markHigerBMI = BMIMark > BMIJohn;
+// console.log(BMIMark, BMIJohn, markHigerBMI);
+
+const firstName = "Sadid";
+const job = "Flutter Developer";
+const birthYear = 1992;
+const year = 2037;
+
+const sadid =
+  " I'm " +
+  firstName +
+  " , a " +
+  (year - birthYear) +
+  " years old " +
+  job +
+  " !";
+console.log(sadid);
+
+const sadidNew = `I'm ${firstName} , a ${year - birthYear} year old ${job} !`;
+console.log(sadidNew);
