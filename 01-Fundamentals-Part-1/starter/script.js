34e7b68db6db989b5eaf9130f46768de6738bd5a
Sadid <sadid.jones@gmail.com>
2022-07-21 23:56:12 +0600

    @21/07/2022

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,4 @@
-// challenge #1
+// // challenge #1
 
 // const massMark = 78;
 // const heightMark = 1.69;
@@ -13,20 +13,32 @@
 // const markHigerBMI = BMIMark > BMIJohn;
 // console.log(BMIMark, BMIJohn, markHigerBMI);
 
-const firstName = "Sadid";
-const job = "Flutter Developer";
-const birthYear = 1992;
-const year = 2037;
+// const firstName = "Sadid";
+// const job = "Flutter Developer";
+// const birthYear = 1992;
+// const year = 2037;
 
-const sadid =
-  " I'm " +
-  firstName +
-  " , a " +
-  (year - birthYear) +
-  " years old " +
-  job +
-  " !";
-console.log(sadid);
+// const sadid =
+//   " I'm " +
+//   firstName +
+//   " , a " +
+//   (year - birthYear) +
+//   " years old " +
+//   job +
+//   " !";
+// console.log(sadid);
 
-const sadidNew = `I'm ${firstName} , a ${year - birthYear} year old ${job} !`;
-console.log(sadidNew);
+// const sadidNew = `I'm ${firstName} , a ${year - birthYear} year old ${job} !`;
+// console.log(sadidNew);
+
+const favorite = Number(prompt("Whats your favorite number"));
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+  console.log(`Cool! ${favorite} is an amazing number`);
+} else if (favorite === 7) {
+  console.log(`Cool! ${favorite} is an amazing number`);
+} else {
+  console.log(`Cool! ${favorite} is an amazing number`);
+}
