ede8749b218a87cf65c2333a2b4b20044c6cf707
Sadid <sadid.jones@gmail.com>
2022-07-23 23:25:28 +0600

    @23/07/2022

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,11 +1,37 @@
-"use strict";
+// "use strict";
 
-let hasDriversLicense = false;
+// let hasDriversLicense = false;
 
-const passTest = true;
+// const passTest = true;
 
-if (passTest) hasDriverLicense = true;
-if (hasDriversLicense) console.log("I can drive");
+// if (passTest) hasDriverLicense = true;
+// if (hasDriversLicense) console.log("I can drive");
 
-const interface = "audio";
-const private = 5374;
+// const interface = "audio";
+// const private = 5374;
+
+// function logger() {
+//   console.log(" My name is Sadid");
+// }
+
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juices = `Juices with ${apples} apples and ${oranges} oranges `;
+//   return juices;
+// }
+// const applesJuices = fruitProcessor(5, 0);
+// console.log(applesJuices);
+// console.log(fruitProcessor(5, 0));
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+function calcAge1(birthYear) {
+  return 2037 - birthYear;
+}
+const calc3 = (birthYear) => {
+  return 2037 - birthYear;
+};
+console.log(calc3(1992));
