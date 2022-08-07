09c2fdc68757ec90c64b99ce30c0585355165660
Sadid <sadid.jones@gmail.com>
2022-08-08 00:09:38 +0600

    @8/8/2022

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,8 @@
 'use strict';
+// Selecting Elements
+const score0EL = document.querySelector('#score--0');
+const score1EL = document.getElementById('score--1');
+const diceEL = document.querySelector('.dice');
+score0EL.textContent = 0;
+score1EL.textContent = 0;
+diceEL.classList.add('hidden');
