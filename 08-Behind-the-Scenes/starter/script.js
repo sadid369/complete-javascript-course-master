a3432864021175476004702ec649ef71e9ceca84
Sadid <sadid.jones@gmail.com>
2022-08-09 16:37:46 +0600

    @9/8/2022

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    const outPut = `${firstName} You are ${age}, bron in ${birthYear}`;
+    console.log(outPut);
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const str = `oh, and you are a millenial, ${firstName}`;
+      console.log(str);
+    }
+    console.log(str);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Sadid';
+calcAge(1992);
+// console.log(age);
+// printAge();
