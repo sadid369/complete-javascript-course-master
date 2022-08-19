6b5042497d18ce338183bab25629f8fa8e100440
Sadid <sadid.jones@gmail.com>
2022-08-19 15:49:51 +0600

    19/08/2022

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -21,28 +21,68 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000);
 */
-const flight = 'LH234';
-const sadid = {
-  name: 'Sadid',
-  passport: 247123789,
-};
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr. ' + passenger.name;
+// const flight = 'LH234';
+// const sadid = {
+//   name: 'Sadid',
+//   passport: 247123789,
+// };
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
 
-  if (passenger.passport === 247123789) {
-    alert('Checked in');
-  } else {
-    alert('Wrong passport');
-  }
-};
+//   if (passenger.passport === 247123789) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
 
 // checkIn(flight, sadid);
 // console.log(flight);
 // console.log(sadid);
 
-const newPassport = function (person) {
-  person.passport = Math.trunc(Math.random() * 1000000);
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 1000000);
+// };
+// newPassport(sadid);
+// checkIn(flight, sadid);
+/*
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
 };
-newPassport(sadid);
-checkIn(flight, sadid);
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original str: ${str}`);
+  console.log(`Transformed string : ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('Javascript is the best', upperFirstWord);
+transformer('Javascript is the best', oneWord);
+
+const high5 = function () {
+  console.log('💚✋🏽 ');
+};
+
+document.body.addEventListener('click', high5);
+
+['a', 'b', 'c'].forEach(high5); */
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet('Hey');
+greeterHey('sadid');
+greet('Hello')('sadid');
+
+const g = gr => n => console.log(`${gr} ${n}`);
+
+g('hey')('sadid');
