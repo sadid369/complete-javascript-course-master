320ac358a4eb5ff41e869b9bff43a0da05b70f84
Sadid <sadid.jones@gmail.com>
2022-08-20 23:06:35 +0600

    @20/08/2022

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -113,5 +113,42 @@ const eurowings = {
 const book = lufthansa.book;
 // book(23666, 'sadid');
 
-book.call(eurowings, 23, 'Sadid  jones');
+book.call(eurowings, 23, 'Sadid  jones1');
 console.log(eurowings);
+
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+
+const bookEW23 = book.bind(eurowings, 23);
+
+bookEW23('Sadid jones 23');
+
+// with event listener
+
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// Partial application
+
+const addTax = (rate, value) => value + value * rate;
+
+console.log(addTax(0.1, 200));
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+
+const at = function (rate, value) {
+  return function () {
+    return value + value * rate;
+  };
+};
+const f = at(0.1, 100);
+const a = f();
+console.log(a);
