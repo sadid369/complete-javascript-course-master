4fe29e5d6a92b01d86fe33bea1be8c13a1650084
Sadid <sadid.jones@gmail.com>
2022-08-26 16:32:24 +0600

    @26/08/2022

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -212,6 +212,45 @@ document
   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 */
 
+// (function () {
+//   console.log('run');
+// })();
+//Example - 1
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+g();
+f();
+h();
+f();
+//Example - 2
+/*const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are three groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+boardPassengers(180, 3);*/
+
 (function () {
-  console.log('run');
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
 })();
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,3 +74,40 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+/*
+let arr = ['a', 'b', 'c', 'd', 'e'];
+//Slice
+const newArr = arr.slice(2);
+const newArrEnd = arr.slice(2, 4);
+const newArrEndMin = arr.slice(-1);
+console.log(newArr);
+console.log(newArrEnd);
+console.log(newArrEndMin);
+console.log(arr.slice());
+console.log([...arr]);
+//Splice
+console.log(arr);
+console.log(arr.splice(-3));
+console.log(arr);
+// console.log(...arr);
+// console.log(arr[0], arr[1], arr[2], arr[3], arr[4]);
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
+// concat
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+//join
+console.log(letters.join('-'));
+*/
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+//getting the last element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
